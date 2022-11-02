02b1586ec6aee65e49bf37e3ef450234ad1a70d1
Gollob <gollob701@gmail.com>
2022-11-03 01:01:08 +0300

    fix

--- test/test_go_to_account_page.py
+++ test/test_go_to_account_page.py
@@ -5,5 +5,5 @@ from selenium.webdriver.support.wait import WebDriverWait
 class Testaccountpage:
     def test_login_by_personal_account_button(self, login):
         login.find_element(By.XPATH, "//p[contains(text(),'Личный Кабинет')]").click()
-        WebDriverWait(login, 3).until(expected_conditions.visibility_of_element_located((By.XPATH, "//header")))
+        WebDriverWait(login, 3).until(expected_conditions.url_contains("https://stellarburgers.nomoreparties.site/account"))
         assert login.current_url == 'https://stellarburgers.nomoreparties.site/account'
--- test/test_logout.py
+++ test/test_logout.py
@@ -1,10 +1,12 @@
 from selenium.webdriver.common.by import By
 import time
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 class Testlogout:
-    def test_test_go_to_account_page(self, login):
+    def test_logout(self, login):
         login.find_element(By.XPATH, "//p[contains(text(),'Личный Кабинет')]").click()
-        time.sleep(1)
+        WebDriverWait(login, 3).until(expected_conditions.visibility_of_element_located((By.XPATH, "//button[contains(text(),'Выход')]")))
         login.find_element(By.XPATH, "//button[contains(text(),'Выход')]").click()
-        time.sleep(1)
+        WebDriverWait(login, 3).until(expected_conditions.visibility_of_element_located((By.XPATH, "//button[contains(text(),'Войти')]")))
         assert login.current_url == 'https://stellarburgers.nomoreparties.site/login'
